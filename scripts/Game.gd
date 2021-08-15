c3664f35fbead99a327c49a934e99fa19b0dd707
Swawm <gulian_kostya@mail.ru>
2021-08-15 15:43:09 +0300

    hard

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -8,15 +8,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
+	if Signals.score > 0:
+		print("0")
+		Engine.time_scale = 1
 	if Signals.score > 20:
 		print("1")
-		ScrollMovement.scroll_speed = 10
+		Engine.time_scale = 1.5
 	if Signals.score > 40:
 		print("2")
-		ScrollMovement.scroll_speed = 20
+		Engine.time_scale = 2.0
 	if Signals.score > 50:
 		print("3")
-		ScrollMovement.scroll_speed = 30
+		Engine.time_scale = 2.5
 	pass # Replace with function body.
 
 
